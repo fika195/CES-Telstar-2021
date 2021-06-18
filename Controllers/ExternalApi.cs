5ca7c1c579a31f5fc03e925cb931a7624076b081
Kasper Brogård Kempf <kbk@netcompany.com>
2021-06-18 08:50:27 +0200

    Final push

---
+++ Controllers/ExternalApi.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Dynamic;
+using TLPD.Models;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+
+namespace TLPD.Controllers
+{​​​​​​​
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExternalApi : ControllerBase
+    {​​​​​​​
+
+
+        [HttpGet]
+        public IActionResult Get(string From, string Destination, int Height, int Length, int Width, int Weight)
+        {​​​​​​​
+            ItemData foo = new ItemData();
+            // calculate price and time
+            foo.Time = 30;
+            foo.Price = 250;
+            string json = Newtonsoft.Json.JsonConvert.SerializeObject(foo);
+            return Ok(json);
+        }​​​​​​​
+    }​​​​​​​
+}
